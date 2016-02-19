aee2234ee13fdf4b954c611cb787c41cc12191c1
Tim Broder <timothy.broder@gmail.com>
2016-02-19 14:17:16 -0500

    LaraVault package skeleton

---
+++ packages/Kidfund/LaraVault/src/LaraVaultServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kidfund\LaraVault;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaraVaultServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
